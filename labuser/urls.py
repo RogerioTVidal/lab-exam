709fee522c94e91d1b9e08850df780153896215d
Rogério Teixeira Vidal <103948082+RogerioTVidal@users.noreply.github.com>
2023-10-06 18:00:16 -0300

    User authentication

--- labuser/urls.py
+++ labuser/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.fRegister, name='register')
+    path('register/', views.fRegister, name='register'),
+    path('login/', views.fLogin, name='login')
 ]
--- labuser/views.py
+++ labuser/views.py
@@ -1,7 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def fRegister(request):
-    return render(request, 'register.html')
+    #print(request.POST)
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    if request.method == 'POST':
+        firstName = request.POST.get('firstName')
+        lastName = request.POST.get('lastName')
+        userName = request.POST.get('userName')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirmPassword = request.POST.get('confirmPassword')
     
+        #return HttpResponse(f'{firstName} - {lastName} - {email}')
+
+        if not password == confirmPassword:
+            messages.add_message(request, constants.ERROR, 'Password and confirmation are not equal.')
+            return redirect('/users/register')
+        if len(password) < 6:
+            messages.add_message(request, constants.ERROR, 'Password must have more than 5 characters.')
+            return redirect('/users/register')
+        
+        try:
+            # Username must be unique!
+            user = User.objects.create_user(
+                first_name = firstName,
+                last_name = lastName,
+                username = userName,
+                email = email,
+                password = password,
+            )
+            messages.add_message(request, constants.SUCCESS, 'Data saved successfuly')
+        except:
+            messages.add_message(request, constants.ERROR, 'Internal error. Please contact admin.')
+            return redirect('/users/register')
+
+        return HttpResponse('Passou')
+    
+def fLogin(request):
+    #return HttpResponse('Login')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+    
+    if user:
+        login(request, user)
+        # Acontecerá um erro ao redirecionar por enquanto, resolveremos nos próximos passos
+        return redirect('/')
+    else:
+        messages.add_message(request, constants.ERROR, 'Invalid user or password')
+        return redirect('/users/login')
